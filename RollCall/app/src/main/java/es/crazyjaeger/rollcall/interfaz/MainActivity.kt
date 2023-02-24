0c6a42dc7ff5dd5acc09f3a7e896ee82434215f9
Crazy Jaeger <miguelgomezcasado@gmail.com>
2023-02-24 11:53:59 +0100

    Annadido fragmento indice

--- RollCall/app/src/main/java/es/crazyjaeger/rollcall/interfaz/MainActivity.kt
+++ RollCall/app/src/main/java/es/crazyjaeger/rollcall/interfaz/MainActivity.kt
@@ -2,11 +2,20 @@ package es.crazyjaeger.rollcall.interfaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import es.crazyjaeger.rollcall.R
+import es.crazyjaeger.rollcall.interfaz.fragments.grupo.IndiceGruposFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.empty_activity)
+        if( savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<IndiceGruposFragment>(R.id.fragment_container)
+            }
+        }
+        setContentView(R.layout.activity_main)
     }
 }
